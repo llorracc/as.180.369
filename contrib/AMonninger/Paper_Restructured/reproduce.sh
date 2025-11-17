f4a98e74321c9c83b60d9644ea758f2d084fe95f
BruceMaoYM <ymao33@jhu.edu>
2025-11-17 17:01:47 -0500

    Revert all BNPL content from AMonninger's Paper_Restructured to original state

--- contrib/AMonninger/Paper_Restructured/reproduce.sh
+++ contrib/AMonninger/Paper_Restructured/reproduce.sh
@@ -1,22 +1,20 @@
-#!/bin/bash  # Use the Bash shell to run this script
+#!/bin/bash
 # Create the environment
-read -r -p "Do you want to create/update your environment? [y/n]" input # Read user input safely (-r) and show prompt (-p)
+read -r -p "Do you want to create/update your environment? [y/n]" input
 
-# Check user response and run the appropriate command
 case $input in
-    [yY][eE][sS]|[yY])  # Match "yes" in any capitalization or a single "y"
-    conda env update --file ./binder/environment.yml   # Update/create the conda environment using this YAML file
-        ;;
+    [yY][eE][sS]|[yY])
+    conda env update --file ./binder/environment.yml 
     ;;
-    [nN][oO]|[nN])  # Match "no" in any capitalization or a single "n"
-    echo "Environment will not be updated" # Display message if user chooses no
+    [nN][oO]|[nN])
+    echo "Environment will not be updated"
     ;;
     esac
 
 # Activate environment
 conda activate ./econ_ark
     
-# Run the Jupyter notebook and execute all cells in place # Register this environment as a Jupyter kernel named "econark"
+# Run the Jupyter notebook and execute all cells in place
 python -m ipykernel install --user --name econark
 jupyter nbconvert --to notebook --execute --inplace 04_dataanalysis.ipynb
 
